a86faf48962bc3f1131dcb934a25e1fab48694da
tmnrwd <61465712+tmnrwd@users.noreply.github.com>
2020-09-15 20:28:26 +0100

    Updates

---
+++ precache-manifest.a039a781c9cdc4f00ba698ab6066f30e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67c5afc76e2722cf742fdccbc496c470",
+    "url": "/todo-express/index.html"
+  },
+  {
+    "revision": "56626db508cab6783bb1",
+    "url": "/todo-express/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "56626db508cab6783bb1",
+    "url": "/todo-express/static/js/2.28b42ad5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/todo-express/static/js/2.28b42ad5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5b6a00c09ab284dbd26",
+    "url": "/todo-express/static/js/main.1829a41d.chunk.js"
+  },
+  {
+    "revision": "6d65cb31fab607b4ebd2",
+    "url": "/todo-express/static/js/runtime-main.598d14e5.js"
+  }
+]);
