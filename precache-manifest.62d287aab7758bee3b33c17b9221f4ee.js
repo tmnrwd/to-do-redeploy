32e830d1bed9f3d59c1da89ad72558c4ac393514
tmnrwd <61465712+tmnrwd@users.noreply.github.com>
2020-09-15 20:46:55 +0100

    Updates

---
+++ precache-manifest.62d287aab7758bee3b33c17b9221f4ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b47f36978e5f71d10998795659699e4",
+    "url": "/to-do-redeploy/index.html"
+  },
+  {
+    "revision": "56626db508cab6783bb1",
+    "url": "/to-do-redeploy/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "56626db508cab6783bb1",
+    "url": "/to-do-redeploy/static/js/2.28b42ad5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/to-do-redeploy/static/js/2.28b42ad5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac42c1b1f0337cac5ad6",
+    "url": "/to-do-redeploy/static/js/main.c8afe696.chunk.js"
+  },
+  {
+    "revision": "cd7e0e379fb2a2bf27be",
+    "url": "/to-do-redeploy/static/js/runtime-main.c14750c0.js"
+  }
+]);
